830927aba4844026a6eedd17b1ed279750eaedf5
TianxiangMa <45935929+TianxiangMa@users.noreply.github.com>
2021-06-15 11:05:52 +0800

    Update generate_fashion_datasets.py
    debug

--- tool/generate_fashion_datasets.py
+++ tool/generate_fashion_datasets.py
@@ -36,30 +36,30 @@ def make_dataset(dir):
 		if lines.endswith('.jpg'):
 			test_images.append(lines)
 
-	for root, _, fnames in sorted(os.walk(os.path.join(dir, 'img_highres'))):
-		for fname in fnames:
-			if is_image_file(fname):
-				path = os.path.join(root, fname)
-				path_names = path.split('/') 
-				print(path_names)
+for root, _, fnames in sorted(os.walk(os.path.join(dir, 'img_highres'))):
+	for fname in fnames:
+		if is_image_file(fname):
+			path = os.path.join(root, fname)
+			path_names = path.split('/') 
+			print(path_names)
 
-				path_names = path_names[6:]
-				
-				path_names[2] = path_names[2].replace('_', '')
-				path_names[3] = path_names[3].split('_')[0] + "_" + "".join(path_names[3].split('_')[1:])
-				path_names = 'fashion' + "".join(path_names)
+			path_names = path_names[6:]
 
-				if path_names in train_images:
-    				img = Image.open(path)
-					img_resize = img.resize([256,256])
-					img_resize.save(os.path.join(train_root, path_names))
-					# shutil.copy(path, os.path.join(train_root, path_names))
-					print(os.path.join(train_root, path_names))
-				elif path_names in test_images:
-    				img = Image.open(path)
-					img_resize = img.resize([256,256])
-					img_resize.save(os.path.join(test_root, path_names))
-					# shutil.copy(path, os.path.join(test_root, path_names))
-					print(os.path.join(test_root, path_names))
+			path_names[2] = path_names[2].replace('_', '')
+			path_names[3] = path_names[3].split('_')[0] + "_" + "".join(path_names[3].split('_')[1:])
+			path_names = 'fashion' + "".join(path_names)
+
+		if path_names in train_images:
+			img = Image.open(path)
+			img_resize = img.resize([256,256])
+			img_resize.save(os.path.join(train_root, path_names))
+			# shutil.copy(path, os.path.join(train_root, path_names))
+			print(os.path.join(train_root, path_names))
+		elif path_names in test_images:
+			img = Image.open(path)
+			img_resize = img.resize([256,256])
+			img_resize.save(os.path.join(test_root, path_names))
+			# shutil.copy(path, os.path.join(test_root, path_names))
+			print(os.path.join(test_root, path_names))
 
 make_dataset('/hd1/matianxiang/MUST/datasets/') # Using yourself root
