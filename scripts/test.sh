e782b7fd7b21c82f43f4416054a6c5c6d756084e
TianxiangMa <45935929+TianxiangMa@users.noreply.github.com>
2021-06-15 12:38:19 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,9 @@
 # pose transfer
 python test.py --dataroot /hd1/matianxiang/MUST/datasets \
-                --dirSem /hd1/matianxiang/MUST/datasets \
                 --pairLst /hd1/matianxiang/MUST/datasets/fashion-resize-pairs-test.csv \
                 --checkpoints_dir /hd1/matianxiang/MUST/check_points \
                 --results_dir ./results \
-                --name MUST-GAN_origin/ \
+                --name MUST-GAN/ \
                 --model MUST --phase test --dataset_mode keypoint --norm instance \
                 --batchSize 1 --resize_or_crop no --gpu_ids 0,1 --BP_input_nc 19 \
                 --which_model_netG MUST \
