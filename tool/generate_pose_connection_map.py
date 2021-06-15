8bab20d221eaeb0c5e14b9aca1e91d5d2e175d6b
TianxiangMa <45935929+TianxiangMa@users.noreply.github.com>
2021-06-15 12:21:53 +0800

    Update generate_pose_connection_map.py
    debug

--- tool/generate_pose_connection_map.py
+++ tool/generate_pose_connection_map.py
@@ -2,7 +2,7 @@ import os.path
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 from PIL import Image
-from tool.getPoseMask import getPoseMask
+from getPoseMask import getPoseMask
 import PIL
 import random
 import pandas as pd
@@ -12,14 +12,14 @@ import json
 
 
 root_path = '/hd1/matianxiang/MUST/datasets/' # Using yourself root
-save_dir = '/hd1/matianxiang/MUST/datasets/pose_connection_map/'
+save_dir = os.path.join(root_path, 'pose_connection_map/')
 os.makedirs(save_dir, exist_ok=True)
 
-annotation_file_train = pd.read_csv('/hd1/matianxiang/MUST/datasets/fashion-resize-annotation-train.csv', sep=':')
-annotation_file_test = pd.read_csv('/hd1/matianxiang/MUST/datasets/fashion-resize-annotation-test.csv', sep=':')
-annotation_file = pd.concat([self.annotation_file_train, self.annotation_file_test], axis=0)
+annotation_file_train = pd.read_csv(os.path.join(root_path, 'fashion-resize-annotation-train.csv'), sep=':')
+annotation_file_test = pd.read_csv(os.path.join(root_path, 'fashion-resize-annotation-test.csv'), sep=':')
+annotation_file = pd.concat([annotation_file_train, annotation_file_test], axis=0)
 
-for i in len(annotation_file):
+for i in range(len(annotation_file)):
     B_row = annotation_file.iloc[i]
     y_cords = json.loads(B_row['keypoints_y'])
     x_cords = json.loads(B_row['keypoints_x'])
