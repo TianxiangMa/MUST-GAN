474bbe3087147f5e7efead5b862af6643926e1bf
TianxiangMa <45935929+TianxiangMa@users.noreply.github.com>
2021-07-28 17:40:49 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,7 @@ python test.py --dataroot /hd1/matianxiang/MUST/datasets \
                 --results_dir ./results \
                 --name MUST-GAN/ \
                 --model MUST --phase test --dataset_mode keypoint --norm instance \
-                --batchSize 1 --resize_or_crop no --gpu_ids 0,1 --BP_input_nc 19 \
+                --batchSize 1 --resize_or_crop no --gpu_ids 0 --BP_input_nc 19 \
                 --which_model_netG MUST \
                 --which_epoch 157 \
 
-# Notes: "--gpu_ids" needs multi-gpus(Such as "--gpu_ids 0,1", only '0' will be used.) even if "batchSize" equals one.
